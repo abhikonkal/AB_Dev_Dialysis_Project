3e865437de19506be739dfd41508d9d1d875291e
reverseabhi <abhikonkal@gmail.com>
2024-07-19 12:56:08 +0530

    paths changed

--- frontend/src/paths/path.js
+++ frontend/src/paths/path.js
@@ -1,2 +1,2 @@
-export const SERVER_PATH="http://localhost:5000"
-export const CLIENT_PATH="http://localhost:3000"
+export const SERVER_PATH="http://ec2-13-51-167-207.eu-north-1.compute.amazonaws.com:5000"
+export const CLIENT_PATH="http://ec2-13-51-167-207.eu-north-1.compute.amazonaws.com:3000"
