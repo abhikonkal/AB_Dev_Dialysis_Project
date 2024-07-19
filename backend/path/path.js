f5d61ca69147f4b674df3aa2005ec1ead4bf8d5f
reverseabhi <abhikonkal@gmail.com>
2024-07-19 14:10:20 +0530

    paths

--- backend/path/path.js
+++ backend/path/path.js
@@ -1,5 +1,5 @@
-const SERVER_PATH="http://ec2-13-51-167-207.eu-north-1.compute.amazonaws.com:5000";
-const CLIENT_PATH="http://ec2-13-51-167-207.eu-north-1.compute.amazonaws.com:3000";
+const SERVER_PATH="https://ec2-16-171-233-174.eu-north-1.compute.amazonaws.com:5000";
+const CLIENT_PATH="https://ec2-16-171-233-174.eu-north-1.compute.amazonaws.com:3000";
 
 module.exports={SERVER_PATH,CLIENT_PATH};
 
--- frontend/src/paths/path.js
+++ frontend/src/paths/path.js
@@ -1,2 +1,2 @@
-export const SERVER_PATH="http://ec2-13-51-167-207.eu-north-1.compute.amazonaws.com:5000"
-export const CLIENT_PATH="http://ec2-13-51-167-207.eu-north-1.compute.amazonaws.com:3000"
+export const SERVER_PATH="https://ec2-16-171-233-174.eu-north-1.compute.amazonaws.com:5000"
+export const CLIENT_PATH="https://ec2-16-171-233-174.eu-north-1.compute.amazonaws.com:3000"
