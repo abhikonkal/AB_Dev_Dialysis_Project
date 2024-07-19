28f991a325c03901dc8062c8fe83869571bcb19b
reverseabhi <abhikonkal@gmail.com>
2024-07-19 12:50:57 +0530

    paths changed

--- backend/path/path.js
+++ backend/path/path.js
@@ -1,4 +1,5 @@
-const SERVER_PATH="http://ec2-13-51-167-207.eu-north-1.compute.amazonaws.com:5000";
-const CLIENT_PATH="http://ec2-13-51-167-207.eu-north-1.compute.amazonaws.com:3000";
+const SERVER_PATH="http://localhost:5000";
+const CLIENT_PATH="http://localhost:3000";
 
 module.exports={SERVER_PATH,CLIENT_PATH};
+
